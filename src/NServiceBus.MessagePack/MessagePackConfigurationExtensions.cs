b42f6a59467332620f2a020f55236ef8d59af35e
Simon Cropp <simon.cropp@gmail.com>
2019-04-13 19:09:22 +1000

    Update MessagePackConfigurationExtensions.cs

--- src/NServiceBus.MessagePack/MessagePackConfigurationExtensions.cs
+++ src/NServiceBus.MessagePack/MessagePackConfigurationExtensions.cs
@@ -32,7 +32,7 @@ namespace NServiceBus
         /// Configures string to use for <see cref="Headers.ContentType"/> headers.
         /// </summary>
         /// <remarks>
-        /// Defaults to "wire".
+        /// Defaults to "messagepack".
         /// </remarks>
         /// <param name="config">The <see cref="SerializationExtensions{T}"/> instance.</param>
         /// <param name="contentTypeKey">The content type key to use.</param>
