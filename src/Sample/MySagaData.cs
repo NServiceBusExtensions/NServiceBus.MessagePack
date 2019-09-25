dfd796378a5f21e90a3b5b7f29d29e3b334851fe
Simon Cropp <simon.cropp@gmail.com>
2019-09-25 21:45:37 +1000

    nullables

--- src/Sample/MySagaData.cs
+++ src/Sample/MySagaData.cs
@@ -5,7 +5,7 @@ public class MySagaData :
     IContainSagaData
 {
     public Guid Id { get; set; }
-    public string Originator { get; set; }
-    public string OriginalMessageId { get; set; }
+    public string? Originator { get; set; }
+    public string? OriginalMessageId { get; set; }
     public Guid TheId { get; set; }
 }
